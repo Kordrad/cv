aba8a9ad5c0abc6f1e40ce6fe0f0fb602ed9fc95
Kordrad <kord.stp@gmail.com>
2020-12-01 21:11:22 +0100

    feat(app): refactor, update data
    change styles
    rename components
    update personal data
    add more vairables for styles
    remove auto resize font

--- src/app/components/ui/category/category.component.ts
+++ src/app/components/ui/category/category.component.ts
@@ -1,16 +1,18 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
-import {ExperienceTitle} from '../../../../domain/enums/experience-title.enum';
+import {ExperienceTitle} from '../../../domain/enums/experience-title.enum';
 
 @Component({
-  selector: 'app-title-experience-section',
-  templateUrl: './title-experience-section.component.html',
-  styleUrls: ['./title-experience-section.component.scss'],
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TitleExperienceSectionComponent {
+export class CategoryComponent {
   @Input() date: string[];
   @Input() title: string;
   @Input() position: string;
+  @Input() git: string;
+  @Input() link: string;
   @Input() type = ExperienceTitle.experience;
 
   readonly ExperienceTitle = ExperienceTitle;
