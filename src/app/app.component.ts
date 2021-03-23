863f4789377e438aa2fba39d1120067f3c52e21d
Kordrad <kord.stp@gmail.com>
2021-03-23 19:45:06 +0100

    fix: change console.log to console.info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { ContactInterface } from './domain/interfaces/contact.interface';
 import { TranslateService } from '@ngx-translate/core';
 import { first } from 'rxjs/operators';
 
-interface Log {
+interface ConsoleInfoInterface {
   text: string;
   style?: string;
 }
@@ -16,31 +16,10 @@ interface Log {
 })
 export class AppComponent implements OnInit {
   private readonly linksKey = 'contact.data';
-
-  constructor(private translateService: TranslateService) {}
-
-  ngOnInit(): void {
-    this.initConsoleLogs();
-  }
-
-  private initConsoleLogs(): void {
-    this.translateService
-      .get(this.linksKey)
-      .pipe(first())
-      .subscribe((links: ContactInterface[]) => {
-        const urls = [];
-        const padding = { text: '\n' };
-        links.forEach(({ link }) => {
-          if (link.includes('http')) {
-            urls.push({ text: link });
-          }
-        });
-
-        const logs: Log[] = [
-          padding,
-          {
-            text: ' Hello!',
-            style: `
+  private readonly consoleInfo: ConsoleInfoInterface[] = [
+    {
+      text: ' Hello!',
+      style: `
               font-weight: bold;
               font-size: 50px;
               color: red;
@@ -53,20 +32,41 @@ export class AppComponent implements OnInit {
                 18px 18px 0 rgb(4,77,145) ,
                 21px 21px 0 rgb(42,21,113)
                `
-          },
-          {
-            text:
-              'Thanks for your interest in my CV.\n' +
-              'Hope you have a nice time checking my site.\n' +
-              'I also encourage you to visit my pages:',
-            style: 'color: rgba(255,255,255,0.5)'
-          },
-          ...urls,
-          padding
-        ];
+    },
+    {
+      text:
+        'Thanks for your interest in my CV.\n' +
+        'Hope you have a nice time checking my site.\n' +
+        'I also encourage you to visit my pages:',
+      style: 'color: rgba(255,255,255,0.5)'
+    }
+  ];
 
-        // eslint-disable-next-line no-console
-        logs.forEach(({ text, style }: Log) => console.log(`%c${text}`, style));
+  constructor(private translateService: TranslateService) {}
+
+  ngOnInit(): void {
+    this.handleLinks();
+  }
+
+  private handleLinks(): void {
+    this.translateService
+      .get(this.linksKey)
+      .pipe(first())
+      .subscribe((links: ContactInterface[]) => {
+        const urls = [];
+        links.forEach(({ link }) => {
+          if (link.includes('http')) {
+            urls.push({ text: link });
+          }
+        });
+        this.displayConsoleInfo(urls);
       });
+  }
+
+  private displayConsoleInfo(urls: ConsoleInfoInterface[]) {
+    const space: ConsoleInfoInterface = { text: '\n' };
+    [space, ...this.consoleInfo, ...urls, space].forEach(({ text, style }) =>
+      console.info(`%c${text}`, style)
+    );
   }
 }
